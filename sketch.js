803d4661fed8a49497a53b63c1b7d4cff1de4903
Wyn Mahler <wyn.m.mahler@gmail.com>
2023-03-09 11:59:10 +0100

    some buttons and interface logic

--- sketch.js
+++ sketch.js
@@ -1,158 +1,84 @@
-let w = 800; 				// width of the fractal
-let h = 600; 				// height of the fractal
-let resolution = 50; 		// can be upped to get higher res results, but color is also tied to this value, gets upped by mrIncrease every mrIterationCounter Iteration
-let mrIncrease = 33; 		// Multiscale Renderering, resolution gets increased by this value
-let maxZoomLevel = 100; 	// how deep you wanna go
-let zoomSpeed = 0.20; 		// how fast you wanna go
+// TODO Start/Stop Zusammenfassen + Resolution Slider + Refresh first generate with new resolution
+const w = 800;
+const h = 600;
+const mrIncrease = 28;
+let resolution = 50;
+let zoomSpeed = 0.15;
 
+let maxZoomLevel = 1;
+let palette;
 let xmin = -2.5;
 let ymin = -2.5;
 let xmax = 2.5;
 let ymax = 2.5;
-let zoom = 2;
 let zoomLevel = 0;
-let mrIterationCounter = 1;
 let targetXmin, targetYmin, targetXmax, targetYmax;
-let ca = new Array(w);
-let cb = new Array(h);
-let data = new Array(w * h);
 let allSets = new Array();
-let palette;
-let colorShifter = 0;
-let colorShifterGoesUp = true;
 let runGenerator = false;
 let counterPlayer = 0;
 let runPlayer = false;
 let brotShown = false;
 let showRefreshButton = true;
-let font;
 let infoDiv;
-let maxZoomLevelSlider, zoomSpeedSlider;
+let zoomSpeedSlider, maxIterationSlider;
 let savedBrightestX = 0;
 let savedBrightestY = 0;
-
-
-function preload() {
-	font = loadFont('assets/RobotoMono-VariableFont_wght.ttf');
-}
 
 function setup() {
 	createCanvas(w, h);
 	pixelDensity(1);
 	colorMode(HSB, 360, 100, 100, 255);
-	textFont(font);
-	textSize(32);
-	fill(255);
-	stroke(8);
-	text('Iterations: ' + maxZoomLevel, 30, 50);
-	text('Resolution: ' + resolution, 30, 100);
-	text('Speed: ' + zoomSpeed, 30, 150);
-	text('Dimensions: ' + w + 'x' + h, 30, 200);
-	const context = canvas.getContext('2d');
+	palette = [color(12, 219, 191), color(208, 255, 12), color(255, 66, 66), color(255, 147, 41), color(232, 0, 255), color(179, 0, 255), color(12, 255, 235), color(255, 0, 221), color(255, 255, 255), color(38, 38, 38)];
+	let context = canvas.getContext('2d');
 	context.imageSmoothingEnabled = true;
 	context.imageSmoothingQuality = 'high';
 	context.canvas.willReadFrequently = true;
-	palette = [
-		color(12, 219, 191),
-		color(208, 255, 12),
-		color(255, 66, 66),
-		color(255, 147, 41),
-		color(232, 0, 255),
-		color(179, 0, 255),
-		color(12, 255, 235),
-		color(255, 0, 221),
-		color(255, 255, 255),
-		color(38, 38, 38)
-	];
-	button = createButton('generate mandelbrot');
-	button.position(30, 250);
-	button.mousePressed(function () {
+	button1 = createButton('generate mandelbrot');
+	button1.position(30, 250);
+	button1.mousePressed(() => {
 		runGenerator = true;
-		button.hide();
+		button1.hide();
+		maxIterationSlider.show();
+		zoomSpeedSlider.show();
+		button2.show();
+		button3.show();
 	});
+	button2 = createButton('start zoom');
+	button2.position(w + 50, 250);
+	button2.hide();
+	button2.mousePressed(() => { maxZoomLevel = maxIterationSlider.value(); runGenerator = true; });
+	button3 = createButton('stop zoom');
+	button3.hide();
+	button3.position(w + 50, 300);
+	button3.mousePressed(() => { runGenerator = false; });
 	zoomSpeedSlider = createSlider(0, 1, zoomSpeed, 0.01);
-	zoomSpeedSlider.position(w + 150, 65);
+	zoomSpeedSlider.position(w + 175, 85);
+	zoomSpeedSlider.hide();
+	maxIterationSlider = createSlider(1, 200, maxZoomLevel);
+	maxIterationSlider.position(w + 175, 50);
+	maxIterationSlider.hide();
+	infoDiv = createDiv('Dimensions: ' + w + 'x' + h + '<br>Iteration: ' + zoomLevel + '/' + maxZoomLevel + '<br>Resolution: ' + resolution + '<br>ZoomSpeed: ' + zoomSpeed);
+	infoDiv.position(w + 20, 30);
 }
 
 function draw() {
 	if (runGenerator) {
-		// set values
-		zoomSpeed = zoomSpeedSlider.value();
-
-		// check resolution
-		if (resolution < 10) {
-			resolution = 10;
-		}
-
-		// remove old info div
-		if (infoDiv) { infoDiv.remove(); }
-
 		// return if maxZoomLevel is reached
 		if (zoomLevel >= maxZoomLevel) {
 			runGenerator = false;
 			return;
 		}
-
 		// show reload button
 		if (showRefreshButton) {
 			showRefreshButton = false;
 			button = createButton('reload page...');
 			button.position(300, height + 30);
-			button.mousePressed(function () {
+			button.mousePressed(() => {
 				window.location.reload();
 			});
 		}
-
-		// colorShift, TODO
-		// if (colorShifter >= 65) { colorShifterGoesUp = false; }
-		// if (colorShifter <= 0) { colorShifterGoesUp = true; }
-		// if (colorShifterGoesUp) { colorShifter++; } else { colorShifter--; }
-		// for (let t = 0; t < palette.length; t++) {
-		// 	let tempColor = palette[t].levels;
-		// 	let r = tempColor[0];
-		// 	let g = tempColor[1];
-		// 	let b = tempColor[2];
-		// 	if (t % 3 == 0) { palette[t] = color(r + colorShifter,g,b); } else if (t % 3 == 1) { palette[t] = color(r,g + colorShifter,b); } else if (t % 3 == 2){ palette[t] = color(r, g, b + colorShifter); }
-		// }
-		// console.log(palette);
-
 		// get pixels array
 		loadPixels();
-
-		// get brightness values of all pixels
-		let brightnessValues = [];
-		for (let i = 0; i < pixels.length; i += 4) {
-			let r = pixels[i];
-			let g = pixels[i + 1];
-			let b = pixels[i + 2];
-			let brightness = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-			brightnessValues.push(brightness);
-		}
-
-		// calculate minimum and maximum brightness values
-		let minBrightnessCC = brightnessValues[0];
-		let maxBrightnessCC = brightnessValues[0];
-		for (let i = 1; i < brightnessValues.length; i++) {
-			if (brightnessValues[i] < minBrightnessCC) {
-				minBrightnessCC = brightnessValues[i];
-			}
-			if (brightnessValues[i] > maxBrightnessCC) {
-				maxBrightnessCC = brightnessValues[i];
-			}
-		}
-
-		// apply contrast stretching to pixels
-		for (let i = 0; i < pixels.length; i += 4) {
-			let r = pixels[i];
-			let g = pixels[i + 1];
-			let b = pixels[i + 2];
-			let brightness = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-			brightness = map(brightness, minBrightnessCC, maxBrightnessCC, 0, 255);
-			pixels[i] = brightness;
-			pixels[i + 1] = brightness;
-			pixels[i + 2] = brightness;
-		}
-
 		//search brightest pixel an zoom in by setting new target xmin/xmax/ymin/ymax
 		let brightestX = 0;
 		let brightestY = 0;
@@ -176,20 +102,24 @@ function draw() {
 		}
 		let cx = map(brightestX, 0, width, xmin, xmax);
 		let cy = map(brightestY, 0, height, ymin, ymax);
-		newWidth = (xmax - xmin) / zoom;
-		newHeight = (ymax - ymin) / zoom;
+		newWidth = (xmax - xmin) / 2;
+		newHeight = (ymax - ymin) / 2;
 		targetXmin = cx - newWidth / 2;
 		targetYmin = cy - newHeight / 2;
 		targetXmax = cx + newWidth / 2;
 		targetYmax = cy + newHeight / 2;
-
-		// Calculations
+		// create Arrays for next Mandelbrot
+		let ca = new Array(w);
+		let cb = new Array(h);
+		let data = new Array(w * h);
+		// map x and y to the new min and max
 		for (let x = 0; x < width; x++) {
 			ca[x] = map(x, 0, width, xmin, xmax);
 		}
 		for (let y = 0; y < height; y++) {
 			cb[y] = map(y, 0, height, ymin, ymax);
 		}
+		// loop through all pixels and write new pixel value in array "data"
 		for (let x = 0; x < width; x++) {
 			for (let y = 0; y < height; y++) {
 				let a = ca[x];
@@ -208,8 +138,36 @@ function draw() {
 				data[x + y * width] = n;
 			}
 		}
-
-		// draw new pixels
+		// get brightness values of all pixels
+		let brightnessValues = [];
+		for (let i = 0; i < pixels.length; i += 4) {
+			let [r, g, b] = pixels.slice(i, i + 3);
+			let brightness = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+			brightnessValues.push(brightness);
+		}
+		// calculate minimum and maximum brightness values
+		let minBrightnessCC = brightnessValues[0];
+		let maxBrightnessCC = brightnessValues[0];
+		for (let i = 1; i < brightnessValues.length; i++) {
+			if (brightnessValues[i] < minBrightnessCC) {
+				minBrightnessCC = brightnessValues[i];
+			}
+			if (brightnessValues[i] > maxBrightnessCC) {
+				maxBrightnessCC = brightnessValues[i];
+			}
+		}
+		// apply contrast stretching to pixels
+		for (let i = 0; i < pixels.length; i += 4) {
+			let r = pixels[i];
+			let g = pixels[i + 1];
+			let b = pixels[i + 2];
+			let brightness = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+			brightness = map(brightness, minBrightnessCC, maxBrightnessCC, 0, 255);
+			pixels[i] = brightness;
+			pixels[i + 1] = brightness;
+			pixels[i + 2] = brightness;
+		}
+		// update the pixels array 
 		for (let i = 0; i < pixels.length; i += 4) {
 			let x = (i / 4) % width;
 			let y = Math.floor((i / 4) / width);
@@ -232,31 +190,21 @@ function draw() {
 				pixels[i + 3] = 190;
 			}
 		}
-
+		// increase zoomLevel Counter
+		zoomLevel++;
+		// push this set to the allSets array
+		allSets.push(pixels);
+		// write updated pixels array to screen
+		updatePixels();
 		// animate with linear interpolation
 		xmin = lerp(xmin, targetXmin, zoomSpeed);
 		ymin = lerp(ymin, targetYmin, zoomSpeed);
 		xmax = lerp(xmax, targetXmax, zoomSpeed);
 		ymax = lerp(ymax, targetYmax, zoomSpeed);
-
-		// increase zoomLevel Counter
-		zoomLevel++;
-
-		// push this set to the allSets array
-		allSets.push(pixels);
-
-		// write updated pixels array to screen
-		updatePixels();
-
-		// display info
-		infoDiv = createDiv('Iteration: ' + zoomLevel + '/' + maxZoomLevel + '<br>' + 'Resolution: ' + resolution + '<br>' + 'ZoomSpeed: ' + zoomSpeed);
-		infoDiv.position(w + 20, 30);
-
 		// Multiscale Rendering resolution increase, modulo 1 = every iteration
 		if (zoomLevel % 1 == 0) {
 			resolution += mrIncrease;
 		}
-
 	} else if (runPlayer) {
 		loadPixels();
 		for (let y = 0; y < height; y++) {
@@ -275,13 +223,17 @@ function draw() {
 			brotShown = false;
 		}
 	}
-
+	// set slider values
+	maxZoomLevel = maxIterationSlider.value();
+	zoomSpeed = zoomSpeedSlider.value();
+	// display info
+	infoDiv.html('Dimensions: ' + w + 'x' + h + '<br>Iteration: ' + zoomLevel + '/' + maxZoomLevel + '<br>Resolution: ' + resolution + '<br>ZoomSpeed: ' + zoomSpeed);
 	// Switch to playmode
-	if (zoomLevel == maxZoomLevel) {
+	if (zoomLevel == maxZoomLevel && zoomLevel != 1) {
 		if (!brotShown) {
 			button = createButton('play the mandelbrot animation!');
 			button.position(30, height + 30);
-			button.mousePressed(function () {
+			button.mousePressed(() => {
 				brotShown = true;
 				counterPlayer = 0;
 				runPlayer = true
